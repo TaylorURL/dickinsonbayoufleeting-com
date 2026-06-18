a7e6aa8cea690d117a22265f0728142db8145e90
Sunday <trenton.taylor.email@gmail.com>
2026-06-18 09:14:49 -0500

    sunday: Write src/index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./app/styles/Theme.css";
 import "./app/styles/index.css";
 import App from "./app/App";
+import { SundayAnalyticsProvider } from "./lib/sunday-analyzer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <SundayAnalyticsProvider siteKey="sa_cbc757334609852911890bb51a781530">
+    <App />
+  </SundayAnalyticsProvider>
+);
