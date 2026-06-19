0f029a6008ea3ed83558b9b68e193639b4eab7a8
Sunday <trenton.taylor.email@gmail.com>
2026-06-19 10:44:13 -0500

    sunday: Write src/app/constants/serviceArea.js

---
+++ src/app/constants/serviceArea.js
@@ -0,0 +1,14 @@
+export const SERVICE_AREA_CITIES = [
+  { name: "San Leon, TX", county: "Galveston County" },
+  { name: "Dickinson, TX", county: "Galveston County" },
+  { name: "Texas City, TX", county: "Galveston County" },
+  { name: "League City, TX", county: "Galveston County" },
+  { name: "La Marque, TX", county: "Galveston County" },
+  { name: "Kemah, TX", county: "Galveston County" },
+  { name: "Bacliff, TX", county: "Galveston County" },
+  { name: "Seabrook, TX", county: "Harris County" },
+  { name: "Galveston, TX", county: "Galveston County" },
+  { name: "Houston, TX", county: "Harris County" },
+  { name: "Pasadena, TX", county: "Harris County" },
+  { name: "Freeport, TX", county: "Brazoria County" },
+];
