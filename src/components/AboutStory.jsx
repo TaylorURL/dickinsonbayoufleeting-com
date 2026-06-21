21fbab8b05937a3fd181d2b09a5509ec0f9c2d65
Sunday <trenton.taylor.email@gmail.com>
2026-06-20 19:27:58 -0500

    sunday: Edit src/components/AboutStory.jsx

--- src/components/AboutStory.jsx
+++ src/components/AboutStory.jsx
@@ -32,7 +32,10 @@ function AboutStory() {
             </p>
           </div>
 
-          <div className="aboutStory__blocks reveal-on-scroll" ref={storyBlocksRef}>
+          <div
+            className="aboutStory__blocks reveal-on-scroll reveal-stagger"
+            ref={storyBlocksRef}
+          >
             {ABOUT_STORY.map((b, idx) => (
               <article key={b.heading} className="aboutStory__block">
                 <span className="aboutStory__idx mono">
